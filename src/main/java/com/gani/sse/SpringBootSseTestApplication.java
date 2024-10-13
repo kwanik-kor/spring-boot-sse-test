2d77e51248db7d41cd9dc3fb16f42b4c95d17397
kwanik-kor <rhksdlr134@naver.com>
2024-10-13 20:16:14 +0900

    chore: 프로젝트 기본 세팅

---
+++ src/main/java/com/gani/sse/SpringBootSseTestApplication.java
@@ -0,0 +1,13 @@
+package com.gani.sse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootSseTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootSseTestApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/gani/sse/SpringBootSseTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gani.sse;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootSseTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
